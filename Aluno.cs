5912044c8ebd2a2c3720b195fb8613dae314f73f
tvidal02 <thiago_acvidal@hotmail.com>
2015-03-30 13:26:47 -0300

    Create Aluno.cs

---
+++ Aluno.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoders.LINQ.Aquecimento
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public double Nota { get; set; }
+
+        public bool DP { get; set; }
+
+        public string Disciplina_DP { get; set; }
+    }
+}
