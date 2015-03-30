a2146e9e97b5885ee958ba40b1d109f95f843558
tvidal02 <thiago_acvidal@hotmail.com>
2015-03-30 13:25:35 -0300

    Create Funcionario.cs

---
+++ Funcionario.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoders.LINQ.Aquecimento
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+
+        public double Salario { get; set; }
+
+        public string Departamento { get; set; }
+    }
+}
