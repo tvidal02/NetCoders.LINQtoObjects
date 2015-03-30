4069d944a13b09ec09e50416b544fddf40cc67e2
tvidal02 <thiago_acvidal@hotmail.com>
2015-03-30 13:24:14 -0300

    Create Pessoa.cs

---
+++ Pessoa.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoders.LINQ.Aquecimento
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public char Sexo { get; set; }
+    }
+}
